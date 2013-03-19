877efe460f040808dbfc218db23dc0ec994527ef
jjbanks <jbanks42@sbcglobal.net>
2013-03-19 13:49:16 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    
+     include('layouts/header.php');
+?>   
+    <div id="main">
+
+ 
+<div style='float: left; margin-left: 20px;'><a href='photo.php?id=images/sailboat.jpg' ><img src='images/sailboat.jpg' alt='photo' width='300' /></a><br /> Sailboat</div>
+<div style='float: left; margin-left: 20px;'><a href='photo.php?id=images/seaturtle.jpg'><img src='images/seaturtle.jpg' alt='photo' width='300' /></a><br />Sea Turtle</div>
+<div style='float: left; margin-left: 20px;'><a href='photo.php?id=images/Paradise-Valley-Mansion.jpg'><img src='images/Paradise-Valley-Mansion.jpg' alt='photo' width='300' />
+</a><br />Paradise Valley Mansion</div>
+<div style='float: left; margin-left: 20px;'><a href='photo.php?id=images/beachfront.jpg'><img src='images/beachfront.jpg' alt='photo' width='300' /></a><br /> Beach Front Relaxation</div>
+<div style='float: left; margin-left: 20px;'><a href='photo.php?id=images/dock.jpg'><img src='images/dock.jpg' alt='photo' width='300' /></a><br />Dock To Island</div>
+    </div>
+<?php  
+    include('layouts/footer.php');
+?>
